ff9fbd74b016a35928ccdffcb3bdfc6f85e84c8b
Daiana Achim <daiana.r.achim@gmail.com>
2022-10-16 23:50:00 +0300

    Metode adugate in controller

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Windows;
+
+namespace Achim_Daiana_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+        /*public IActionResult Index()
+        {
+            return View();
+           
+        }*/
+
+        
+        public string Index()
+        {
+            string message = "Bine ai venit pe acesta platforma!";
+            return message;
+        }
+
+        
+        public string Welcome()
+        {
+            string salute = "Salut!";
+            return salute;
+        }
+
+       
+        public string WelcomeUser(string name, int id)
+        {
+            name = "Daiana";
+            id = 1;
+            return "Salut " + name + ",id-ul tau este: " + id;
+        }
+    }
+}
